13f2728b102dc0e0b49ef70f8636492cbfd47507
Murillo Oliveira <00001106481720SP@al.educacao.sp.gov.br>
2024-11-19 15:25:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Soul society',
+    'O que faz um Shinigami?',
+    'O Shinigami purifica os hololows'
+)
+
+criaCartao(
+    'Mundo real ',
+    'Onde o Ichigo mora?',
+    'Karakura'
+)
+
+criaCartao(
+    'Almas',
+    'O que é um hollow?',
+    'Uma alma oca que vaga por aí'
+)
+
+criaCartao(
+    'Armamento',
+    'O que é Zanpakuto?',
+    'espada do shinigami'
+)
